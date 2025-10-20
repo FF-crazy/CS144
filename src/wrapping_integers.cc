6abbeebf92936f9a39f842a1b35f96f48e33de4b
FF-crazy <13453488571@163.com>
2025-10-20 13:42:38 +0000

    refeat: imporve logic

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -9,15 +9,14 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  auto absolute = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_);
-  absolute += checkpoint & 0xFFFFFFFF00000000;
-  auto flag = (1UL << 32);
-  if (absolute < flag && checkpoint < absolute) {
-    return absolute;
+  auto absolute = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_) + (checkpoint & 0xFFFFFFFF00000000);
+  if (absolute > checkpoint && absolute - checkpoint > (1UL << 31)) {
+    if (absolute >= (1UL << 32)) {
+      return absolute - (1UL << 32);
+    }
   }
-  if (checkpoint < absolute) {
-    return checkpoint + flag - absolute < absolute - checkpoint ? absolute - flag : absolute;
-  } else {
-    return checkpoint - absolute < absolute + flag - checkpoint ? absolute : absolute + flag;
+  if (checkpoint > absolute && checkpoint - absolute >= (1UL << 31)) {
+    return absolute + (1UL << 32);
   }
+  return absolute;
 }
