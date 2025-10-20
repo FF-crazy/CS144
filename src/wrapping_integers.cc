3decf5dcc2e52020433aa0205e9802add6614c62
FF-crazy <13453488571@163.com>
2025-10-20 13:31:31 +0000

    feat: warp32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,20 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  auto absolute = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_);
+  absolute += checkpoint & 0xFFFFFFFF00000000;
+  auto flag = (1UL << 32);
+  if (absolute < flag && checkpoint < absolute) {
+    return absolute;
+  }
+  if (checkpoint < absolute) {
+    return checkpoint + flag - absolute < absolute - checkpoint ? absolute - flag : absolute;
+  } else {
+    return checkpoint - absolute < absolute + flag - checkpoint ? absolute : absolute + flag;
+  }
 }
