e6962dd8ed1cc87d2144768b658bd42a11f0d807
FF-crazy <13453488571@163.com>
2025-10-21 02:41:33 +0000

    refeat: isn now with correct number

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,29 +4,34 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  if (message.RST || reader().is_finished()) {
+  if ( message.RST ) {
     reassembler_.reader().set_error();
     return;
   }
-  uint64_t index = 0;
-  if (message.SYN) {
-    isn_ = message.seqno + 1;
-  } else {
-    if (!isn_.has_value() || message.seqno + 1 == isn_) {
-      return;
-    }
-    auto checkpoint = writer().bytes_pushed();
-    index = message.seqno.unwrap(isn_.value(), checkpoint);
+  if ( writer().is_closed() ) {
+    return;
   }
-  reassembler_.insert(index, message.payload, message.FIN);
+  uint64_t index = 0;
+  if ( message.SYN ) {
+    isn_ = message.seqno;
+  }
+  if ( !isn_.has_value() ) {
+    return;
+  }
+  auto checkpoint = writer().bytes_pushed();
+  index = message.seqno.unwrap( isn_.value(), checkpoint );
+  if ( !message.SYN && index == 0 ) {
+    return;
+  }
+  reassembler_.insert( index - 1 + message.SYN, message.payload, message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   optional<Wrap32> ackno = {};
-  if (isn_.has_value()) {
-    ackno = Wrap32::wrap(writer().bytes_pushed(), isn_.value()) + writer().is_closed();
+  if ( isn_.has_value() ) {
+    ackno = Wrap32::wrap( writer().bytes_pushed() + writer().is_closed() + 1, isn_.value() );
   }
   uint16_t window_size = writer().available_capacity() < UINT16_MAX ? writer().available_capacity() : UINT16_MAX;
-  return {ackno, window_size, reader().has_error()};
+  return { ackno, window_size, reader().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -9,14 +9,15 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  auto absolute = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_) + (checkpoint & 0xFFFFFFFF00000000);
-  if (absolute > checkpoint && absolute - checkpoint > (1UL << 31)) {
-    if (absolute >= (1UL << 32)) {
-      return absolute - (1UL << 32);
+  auto absolute
+    = static_cast<uint64_t>( this->raw_value_ - zero_point.raw_value_ ) + ( checkpoint & 0xFFFFFFFF00000000 );
+  if ( absolute > checkpoint && absolute - checkpoint > ( 1UL << 31 ) ) {
+    if ( absolute >= ( 1UL << 32 ) ) {
+      return absolute - ( 1UL << 32 );
     }
   }
-  if (checkpoint > absolute && checkpoint - absolute >= (1UL << 31)) {
-    return absolute + (1UL << 32);
+  if ( checkpoint > absolute && checkpoint - absolute >= ( 1UL << 31 ) ) {
+    return absolute + ( 1UL << 32 );
   }
   return absolute;
 }
